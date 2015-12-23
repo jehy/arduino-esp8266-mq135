d5abf3779f1fe84159d68422d8686c0226d393ec
Jehy <fate@jehy.ru>
2015-12-23 13:41:58 +0300

    first version of transmitting co2 data to my server

---
+++ mq135-data.h
@@ -0,0 +1,28 @@
+
+#define MQ135_DEFAULTPPM 399 //default ppm of CO2 for calibration
+#define MQ135_DEFAULTRO 68550 //default Ro for MQ135_DEFAULTPPM ppm of CO2
+#define MQ135_SCALINGFACTOR 116.6020682 //CO2 gas value
+#define MQ135_EXPONENT -2.769034857 //CO2 gas value
+#define MQ135_MAXRSRO 2.428 //for CO2
+#define MQ135_MINRSRO 0.358 //for CO2
+
+
+/*
+ * get the calibrated ro based upon read resistance, and a know ppm
+ */
+long mq135_getro(long resvalue, double ppm) {
+return (long)(resvalue * exp( log(MQ135_SCALINGFACTOR/ppm) / MQ135_EXPONENT ));
+}
+
+/*
+ * get the ppm concentration
+ */
+double mq135_getppm(long resvalue, long ro) {
+double ret = 0;
+double validinterval = 0;
+validinterval = resvalue/(double)ro;
+//if(validinterval<MQ135_MAXRSRO && validinterval>MQ135_MINRSRO) {
+ret = (double)MQ135_SCALINGFACTOR * pow( ((double)resvalue/ro), MQ135_EXPONENT);
+//}
+return ret;
+}
---
+++ mq135-wifi.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+//#include <DNSServer.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <Wire.h>
+#include "mq135-data.h"
+#include "wifi-utils.h"
+#include "wifi-creds.h"
+
+
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true)
+    delay(10000);
+  }
+
+  
+ // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:    
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+   
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+}
+
+
+
+void loop() {
+  //dns.processNextRequest();  
+  //server.handleClient();
+  
+  uint16_t valr = analogRead(A0);
+  uint16_t val =  ((float)22000*(1023-valr)/valr); 
+  float mq135_ro = mq135_getro(15231, 660);//8000;//mq135_getro(val, 500);
+  //convert to ppm (using default ro)
+  float valAIQ = mq135_getppm(val, mq135_ro);
+  
+  delay(10000);
+  printCurrentNet();
+    Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect("co2.jehy.ru", 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /send.php?data={\"id\":1,\"val\":"+String(valr)+",\"ppm\":"+String(valAIQ)+"} HTTP/1.1");
+    client.println("Host: co2.jehy.ru");
+    client.println("Connection: close");
+    client.println();
+    client.stop();
+  }
+}
+
+
