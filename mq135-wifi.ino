946bed4ddb99e1a847a8bce3757e04e06fe08c05
Eugene Bond <fate@jehy.ru>
2016-01-07 10:35:54 +0300

    correct data after c2h5oh error

--- mq135-wifi.ino
+++ mq135-wifi.ino
@@ -13,8 +13,8 @@ char macStr[20];
 long previousMillis = 0;        // will store last time LED was updated
 long interval = 5000;           // interval at which to blink (milliseconds)
 
-int temp=20;
-int humidity=27;
+int temp=21;
+int humidity=26;
 
 void setup() {
   ///wdt_enable(WDTO_8S);
@@ -74,7 +74,7 @@ void loop() {
   }
   
   long val =  ((float)22000*(1023-valr)/valr); 
-  long mq135_ro = mq135_getro(43615,1274);//(75617, 1200);//8000;//mq135_getro(val, 500);
+  long mq135_ro = mq135_getro(58378, 1050);//8000;//mq135_getro(val, 500);
   //convert to ppm (using default ro)
   float valAIQ = mq135_getppm(val, mq135_ro);
 
